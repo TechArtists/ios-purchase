b06f2b2ddb1298b0fe13539fb0c44c1cf2bef283
Tataru Robert <robert.tataru98@gmail.com>
2025-03-31 15:02:17 +0300

    Make dates in Granted Entitlement optional

--- Sources/TAPurchasing/Models/TAGrantedEntitlement.swift
+++ Sources/TAPurchasing/Models/TAGrantedEntitlement.swift
@@ -14,16 +14,16 @@ public struct TAGrantedEntitlement: Sendable, Identifiable {
     /// Identifies the product the transaction is for.
     public let productID: String
     /// The date this transaction occurred on.
-    public let latestPurchaseDate: Date
+    public let latestPurchaseDate: Date?
     /// The date the original transaction for `productID` or`subscriptionGroupID` occurred on.
-    public let originalPurchaseDate: Date
+    public let originalPurchaseDate: Date?
     /// The date the users access to `productID` expires
     /// - Note: Only for subscriptions.
     public let expirationDate: Date?
     /// Checks that entitlement is active
     public let isActive: Bool
     
-    public init(id: String, productID: String, latestPurchaseDate: Date, originalPurchaseDate: Date, expirationDate: Date?, isActive: Bool) {
+    public init(id: String, productID: String, latestPurchaseDate: Date?, originalPurchaseDate: Date?, expirationDate: Date?, isActive: Bool) {
         self.id = id
         self.productID = productID
         self.latestPurchaseDate = latestPurchaseDate
